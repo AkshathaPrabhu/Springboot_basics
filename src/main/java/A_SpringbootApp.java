631a4b623ae3752681c63602fd148c98ce0e6a5a
Akshatha.Prabhu <akshatha.prabhu@target.com>
2019-03-24 21:16:05 +0530

    correcting project structure, added HelloController

--- src/main/java/A_SpringbootApp.java
+++ src/main/java/A_SpringbootApp.java
@@ -1,4 +1,4 @@
-package main;
+package main.java;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
@@ -11,11 +11,11 @@ import org.springframework.boot.SpringApplication;
  * @SpringBootApplication - will tell spring that this class is the starting point for our app */
 
 @SpringBootApplication
-public class SpringbootApp {
+public class A_SpringbootApp {
     public static void main(String[] args) {
 
         //this line will tell Springboot that a servlet container needs to be created and this app needs to be hosted in this servlet
         // the 'static method run' helps with this and takes in two args : run(<the springboot class>, <command line args>)
-        SpringApplication.run(SpringbootApp.class, args);
+        SpringApplication.run(A_SpringbootApp.class, args);
     }
 }
